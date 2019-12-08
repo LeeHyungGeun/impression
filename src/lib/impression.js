2cbd0022be603d8f71f573e1f50874502dcd7bbe
LeeHyungGeun <lhg4031@gmail.com>
2019-12-08 23:39:48 +0900

    Add a impression logic

--- src/lib/impression.js
+++ src/lib/impression.js
@@ -17,7 +17,40 @@ class Impression {
     }
 
     this._el = el
+
+    Impression._instances.push(el)
+
+    this._check()
+  }
+
+  _check() {
+    setInterval(() => {
+      const el = Impression._instances[0]
+      const { top: elementTop, left: elementLeft, bottom: elementBottom, right: elementRight } = el.getBoundingClientRect()
+
+      // window.innerHeight // 내부 height
+      // window.innerWidth // 내부 width
+      // window.pageYOffset // 스크롤된 길이
+
+      // const screenLeft = window.pageXOffset
+      // const screenTop = window.pageYOffset
+      // const screenRight = screenLeft + window.innerHeight
+      // const screenBottom = screenTop + window.innerWidth
+
+      if (elementTop >= 0 &&
+        elementLeft >= 0 &&
+        elementBottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        elementRight <= (window.innerWidth || document.documentElement.clientWidth)) {
+        console.log('in')
+      } else {
+        console.log('out')
+      }
+
+    }, 1000)
   }
 }
+
+// Impression._instances = new WeakMap()
+Impression._instances = []
 
 export default Impression
---
+++ test/unit/lib/impression.js
@@ -0,0 +1,14 @@
+// import Impression from '@/lib/impression'
+
+// describe('lib/impression', () => {
+//   beforeEach(() => {
+//     document.body.innerHTML = `
+//       <div id="wrap-1"></div>
+//     `
+//   })
+
+//   it('impression.constructor', () => {
+//     // Given
+//     new Impression()
+//   })
+// })
